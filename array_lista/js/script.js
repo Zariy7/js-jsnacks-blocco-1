0a0e160cab7669b11cf6b2287f87702aaf6fdbbf
Zariy7 <nazariy.hereha@gmail.com>
2023-11-27 00:40:12 +0100

    array_lista completed

---
+++ array_lista/js/script.js
@@ -0,0 +1,26 @@
+let frigorifero = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino',    , 'limone', 'fragola'];
+let frutto = 'pesca';
+
+//ALTERNATIVE 1 = USING SPLICE
+//frigorifero.splice(frigorifero.length, 0, frutto);
+//console.log(frigorifero);
+
+//ALTERNATIVE 2 = ADDING A VALUE DIRECTLY
+frigorifero [frigorifero.length] = frutto;
+console.log(frigorifero);
+
+frutto = 'cocomero';
+flag = false;
+
+for(let i=0; i<frigorifero.length; i++){
+    if(frigorifero[i] === frutto){
+        flag = true;
+    }
+}
+
+if(flag){
+    console.log('Trovato! Devo solo preparare il cocktail.');
+}
+else{
+    console.log('Oh no, devo uscire a comprare il cocomero!');
+}
